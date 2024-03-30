eeaa2e0be6d282f703c6547cf19eb1b8d38a889f
mkabdelrahman <mkcodedev@gmail.com>
2024-03-30 03:54:33 +0200

    add external deps to  the image

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
 	fileServer := http.FileServer(http.Dir("./static"))
 
-	http.Handle("/", fileServer)
+	r.Handle("/", fileServer)
 
 	log.Println("starting server on addr", ":3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
